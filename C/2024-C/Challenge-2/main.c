82cf364987ab1aadfc1d08206f66e0c642ac7982
Frank Qian <frankqian1993@gmail.com>
2024-10-16 05:50:59 +1030

    add challenge 1,2 and 3 update

---
+++ C/2024-C/Challenge-2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+  // printf("Frank Qian\n");
+  // printf("My age is 20\n");
+  // printf("Gender: Male\n");
+  printf("Frank Qian\nMy age is 20\nGender: Male\n");
+}
